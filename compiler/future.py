4f678fddc1bcb5c17122e6929711984d9deeba17
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-01-04 19:31:25 +0200

    future: Update for Python3 ast module.

--- compiler/future.py
+++ compiler/future.py
@@ -3,13 +3,14 @@
 """
 from __future__ import print_function
 
-from compiler import ast, walk
+import ast
+from compiler import walk
 
 def is_future(stmt):
     """Return true if statement is a well-formed future statement"""
-    if not isinstance(stmt, ast.From):
+    if not isinstance(stmt, ast.ImportFrom):
         return 0
-    if stmt.modname == "__future__":
+    if stmt.module == "__future__":
         return 1
     else:
         return 0
@@ -24,14 +25,14 @@ class FutureParser:
         self.found = {} # set
 
     def visitModule(self, node):
-        stmt = node.node
-        for s in stmt.nodes:
+        for s in node.body:
             if not self.check_stmt(s):
                 break
 
     def check_stmt(self, stmt):
         if is_future(stmt):
-            for name, asname in stmt.names:
+            for alias in stmt.names:
+                name = alias.name
                 if name in self.features:
                     self.found[name] = 1
                 else:
@@ -48,10 +49,10 @@ class FutureParser:
 class BadFutureParser:
     """Check for invalid future statements"""
 
-    def visitFrom(self, node):
+    def visitImportFrom(self, node):
         if hasattr(node, 'valid_future'):
             return
-        if node.modname != "__future__":
+        if node.module != "__future__":
             return
         raise SyntaxError("invalid future statement " + repr(node))
 
