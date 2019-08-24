00a4b3cc747727ed7be5bb18fec4e3dded42c488
Skip Montanaro <skip.montanaro@gmail.com>
2019-08-23 20:58:09 -0500

    more detailed output

--- test_testcorpus_run.py
+++ test_testcorpus_run.py
@@ -1,13 +1,73 @@
 #!/usr/bin/env python3
-#
-# Compare "compiler" package output against reference data produced
-# by test_testcorpus_prepare.py.
-#
+
+"""Compare "compiler" package output against reference data produced
+by test_testcorpus_prepare.py.
+
+usage: {PROG} [ -h ] [ -v ] [ testfile ... ]
+
+-v - produce more verbose output
+-h - print usage info and exit
+
+If no test files are given on the command line, all files matching
+testcorpus/*.py are run.
+"""
+
+import getopt
 import glob
-from subprocess import check_call
+import os
+from subprocess import check_call, CalledProcessError
+import sys
 
+PROG = os.path.basename(sys.argv[0])
 
-for t in sorted(glob.glob("testcorpus/*.py")):
-    print(t)
-    check_call("./python3.5-nopeephole compiler_runtest.py %s > %s.out" % (t, t), shell=True)
-    check_call("diff -u %s.exp %s.out" % (t, t), shell=True)
+def main():
+    args = sys.argv[1:]
+    verbose = False
+    opts, args = getopt.getopt(args, "vh")
+    for opt, _arg in opts:
+        if opt == "-v":
+            verbose = True
+        elif opt == "-h":
+            usage()
+            return 0
+
+    runs = failures = passes = 0
+
+    testfiles = sorted(sys.argv[1:])
+    if not testfiles:
+        testfiles = sorted(glob.glob("testcorpus/*.py"))
+
+    try:
+        for testfile in testfiles:
+            print(testfile, end=" ", file=sys.stderr)
+            runs += 1
+            try:
+                check_call("./python3.5-nopeephole compiler_runtest.py"
+                           f" {testfile} > {testfile}.out",
+                           shell=True)
+            except CalledProcessError as exc:
+                print("FAIL", file=sys.stderr)
+                if verbose:
+                    print(exc, file=sys.stderr)
+                failures += 1
+                continue
+            try:
+                check_call(f"diff -u {testfile}.exp {testfile}.out", shell=True)
+            except CalledProcessError as exc:
+                print("FAIL", file=sys.stderr)
+                if verbose:
+                    print(exc, file=sys.stderr)
+                failures += 1
+            else:
+                print("PASS", file=sys.stderr)
+                passes += 1
+    finally:
+        print("tests:", runs, "passes:", passes, "failures:", failures)
+
+    return failures
+
+def usage():
+    print(__doc__.format(**globals()).strip(), file=sys.stderr)
+
+if __name__ == "__main__":
+    sys.exit(main())
